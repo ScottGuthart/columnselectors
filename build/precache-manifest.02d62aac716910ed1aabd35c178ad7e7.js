71767c78a48b8a95b380fd617d7c49451e811805
Scott Guthart <seguthart@gmail.com>
2020-10-30 22:06:12 -0400

    added the build directory to source control

---
+++ build/precache-manifest.02d62aac716910ed1aabd35c178ad7e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b18055cd8dbb3fd65b2aa3673e8c4d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe05f61b34e5f72d9ead",
+    "url": "/static/css/main.3c0a6599.chunk.css"
+  },
+  {
+    "revision": "49a2498b6f93cd30cba7",
+    "url": "/static/js/2.c1ed20cc.chunk.js"
+  },
+  {
+    "revision": "226cb6491056a6a9a4dd6fe6cc73cac9",
+    "url": "/static/js/2.c1ed20cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe05f61b34e5f72d9ead",
+    "url": "/static/js/main.5d8f3498.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  }
+]);
